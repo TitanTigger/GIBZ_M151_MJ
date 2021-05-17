98e73c8728320df53b009a05a5ed6a78cb10af5c
Max Braten <max.braaten03@gmail.com>
2021-05-17 11:01:24 +0200

    Changes to base structure
    Removed the API Layer and changed the presentation layer to a default mvc project.

---
+++ GIBZ_M151_ToDoList_2021/L_Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace L_Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
